4ca05dae75fc9de05b24c2df47c4df1d9c256a85
rayshoo <fire@dragonz.dev>
2023-09-26 01:28:29 +0000

    [ADD] add caller identity for peer owner id

--- main.tf
+++ main.tf
@@ -1,6 +1,9 @@
+data "aws_caller_identity" "current" {}
+
 resource "aws_vpc_peering_connection" "connections" {
   for_each    = { for peer_vpc in var.peer_vpcs : peer_vpc.name => peer_vpc }
   vpc_id      = "${var.vpc_id}"
+  peer_owner_id = "${data.aws_caller_identity.current.account_id}"
   peer_vpc_id = "${each.value.id}"
   auto_accept = each.value.auto_accept
 }
