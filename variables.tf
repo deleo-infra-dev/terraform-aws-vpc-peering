3bea1aaef090c7831e5800233383f4e193e6ca46
chan <bcchoi@deleo.co.kr>
2024-01-23 02:32:41 +0000

    [UPDATE] peering name var default value add

--- variables.tf
+++ variables.tf
@@ -1,18 +1,17 @@
 variable "vpc_name" {
   description = "vpc name"
-  type        = any
-  default     = {}
+  type        = string
 }
 
 variable "vpc_id" {
   description = "vpc id"
-  type        = any
-  default     = {}
+  type        = string
 }
 
 variable "vpc_peering_name" {
   description = "vpc peering name"
   type        = string
+  default     = ""
 }
 
 variable "peer_vpcs" {
